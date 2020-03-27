0fc360c592a4ab096595d61afbccce4313db12cf
Swen <99058148@mydavinci.nl>
2020-03-27 14:30:50 +0100

    Commit4

--- vormgeving/character.php
+++ vormgeving/character.php
@@ -24,37 +24,42 @@ $result = $query->fetchall();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Character - Bowser</title>
+    <title>Character</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link href="resources/css/style2.css" rel="stylesheet"/>
+    <link href="resources/css/style.css" rel="stylesheet">
 </head>
 <body>
 <header>
     <a class="backbutton" href="index.php"><i class="fas fa-long-arrow-alt-left"></i> Terug</a></header>
-<div id="container">
+    <div id="container">
     <div class="detail">
-        <div class="left">
 
-            </div>
-        </div>
-    <div class="right">
-         <?php
+        <?php
             foreach ($result as $name){
          ?>
 
-            <h1 style="..."><?php echo $name['name']?></h1>
+         <h1 style="..."><?php echo $name['name']?></h1>
          <img src="images/<?php echo $name["avatar"]?>" id="img" style="width:200px; height:200px; border-radius:50%">
+         <div id="informatie" style="background-color: <?php echo $name['color']?>">
 
         <?php
-                echo  " &emsp;"."<i class=\"fas fa-heart\"></i>".$name['health'] ." &emsp;"."<i class=\"fas 
-                fa-fist-raised\"></i>" . $name['attack'] . " &emsp;"."<i class=\"fas fa-shield-alt\"></i>" 
-                . $name['defense'] . "<br>" . "<br>";   
+                echo  " &emsp;"."<i class=\"fas fa-heart\"></i>".$name['health']."<br>"." &emsp;"."<i class=\"fas 
+                fa-fist-raised\"></i>" . $name['attack']."<br>" . " &emsp;"."<i class=\"fas fa-shield-alt\"></i>" 
+                . $name['defense'] . "<br>" . "<br>" . $name['weapon'] . "<br>" . $name['armor'] . $name['color'];   
           }?>
 
+        <div class="left">
+         </div>
+
+
         </div>
-        <div>
-            <p><?php echo $name["bio"]?></p>
         </div>
+   
+         <div class="right">
+            <p><?php echo $name['bio']?></p>
+         </div>
+        
+
 </div>
 <footer>&copy; [Swen Sperling] 2020</footer>
 </body>
--- vormgeving/index.php
+++ vormgeving/index.php
@@ -25,7 +25,7 @@ $result = $query->fetchall();
     <meta charset="UTF-8">
     <title>All Characters</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link href="resources/css/style2.css" rel="stylesheet">
+    <link href="resources/css/style.css" rel="stylesheet">
 </head>
 <body>
 <header><h1>Alle [10] characters uit de database</h1>
@@ -37,7 +37,6 @@ foreach ($result as $name) {
     ?>
 
     <img src="images/<?php echo $name["avatar"] ?>" id="img" style="width: 100px; height: 100px; border-radius: 50%" >
-    
     <?php
      echo "<a href='character.php?id={$name['id']}' href='character.php'>". $name['name']."</a>"." &emsp;"."<i class=\"fas fa-heart\"></i>".$name['health'] ." &emsp;"."<i class=\"fas fa-fist-raised\"></i>" . $name['attack'] . " &emsp;"."<i class=\"fas fa-shield-alt\"></i>" . $name['defense'] . "<br>" . "<br>";
     }?>
